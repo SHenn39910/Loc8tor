c625547fe6972df4addd33ee46345c4249d0440d
Sonja E. Hennessy <sonjahennessy15@gmail.com>
2021-07-13 17:24:11 -0400

    created mixins

--- Loc8tor/app_server/controllers/locations.js
+++ Loc8tor/app_server/controllers/locations.js
@@ -5,7 +5,28 @@ const homeList = (req, res) =>{
         pageHeader: {
             title: 'Loc8tor',
             strapline: 'Find places to work with wifi near you!'
-        }});
+        },
+        locations: [{
+            name: 'Queencups',
+            address: '1 Main Street, Connecticut, RG6 1PS',
+            rating: 5,
+            facilities: ['Coffee', 'Food', 'Premium wifi'],
+            distance: '100m'
+        }, {
+            name: 'Cafe Blackrock',
+            address: '38 Top Road, Connecticut RG6 1PS',
+            rating: 4,
+            facilities: ['Coffee', 'Hot Drinks', 'Wifi'],
+            distance: '200m'
+
+        },{
+            name: 'Webster Arena',
+            address: '331 Atlantic Street, Connecticut RG6 1PS',
+            rating: 1,
+            facilties: ['Food', 'Drinks', 'Wifi'],
+            distance: '10m'
+        }
+        ]});
 };
 
 
